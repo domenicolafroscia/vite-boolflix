d4890baf0704a02cd8a877813fafe65991d01e1f
domenicolafroscia <domenicolafroscia@gmail.com>
2023-11-29 14:06:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+import { store } from "./store.js";
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+    data() {
+        return {
+          store,
+        };
+    },
+    components: { AppHeader }
+};
+
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss"
+
+
+</style>
